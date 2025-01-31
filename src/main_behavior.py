beb9456b83dd799455b38d26a532d855e0a34a1f
Patricia Jaques <patricia.jaques@gmail.com>
2025-01-31 08:19:57 -0300

    melhorando algoritmo de visualização

--- src/main_behavior.py
+++ src/main_behavior.py
@@ -16,6 +16,6 @@ def main():
     pipeline.run()
 
 if __name__ == "__main__":
-    output_dir = "/Users/patricia/Documents/code/python-code/behavior-detection/output"
-    file_utils.clear_output_directory(output_dir)
+    # output_dir = "/Users/patricia/Documents/code/python-code/behavior-detection/output"
+    # file_utils.clear_output_directory(output_dir)
     main()
